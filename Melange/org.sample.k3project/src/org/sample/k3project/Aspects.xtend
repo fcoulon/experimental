c5c40570d29d252fe8d583687afc4a9972baf3fb
Fabien Coulon <fabien.coulon@inria.fr>
2016-05-18 16:01:45 +0200

    Add Mass.getSubComponentsObjects()

--- Melange/org.sample.k3project/src/org/sample/k3project/Aspects.xtend
+++ Melange/org.sample.k3project/src/org/sample/k3project/Aspects.xtend
@@ -2,6 +2,17 @@ package org.sample.k3project
 
 import org.polarsys.capella.core.data.cs.Part
 import fr.inria.diverse.k3.al.annotationprocessor.Aspect
+import java.util.List
+import org.polarsys.capella.core.data.cs.Component
+import org.eclipse.emf.ecore.EObject
+import org.eclipse.emf.common.util.BasicEList
+import org.eclipse.emf.common.util.EList
+import org.polarsys.capella.core.data.cs.AbstractDeploymentLink
+import org.polarsys.capella.core.data.capellacore.Feature
+import org.polarsys.capella.core.data.pa.PhysicalComponent
+import org.polarsys.capella.core.data.cs.impl.PartImpl
+import org.polarsys.capella.core.data.cs.CsPackage
+import org.polarsys.capella.core.data.cs.CsFactory
 
 @Aspect(className=Part)
 class Mass{
@@ -29,10 +40,64 @@ class Mass{
 	}
 	
 	def int computeMass(){
-		val subParts = _self.eAllContents.filter(Part)
+		val subParts = _self.getSubComponentsObjects
 		val subPartWeight = subParts.map[computeMass].reduce[w1, w2| w1+w2]
 		
 		return subPartWeight + _self.value
+	}
+	
+	def EList<Part> getSubComponentsObjects(){
+		val list = new BasicEList<Part>();
+
+		var EObject type = null
+		
+		var EList<AbstractDeploymentLink> deployedLinkList = null
+		
+		if (_self instanceof  Part)
+		{
+			type = _self.getAbstractType()
+			deployedLinkList = _self.getDeploymentLinks()
+			
+		}
+		else 
+		{
+			if (_self instanceof Component) 
+				type = _self;
+		}
+		
+		// Traitement des composants
+		if (type != null && type instanceof  Component)
+		{
+			val Component pc = type as Component
+			for (Feature feature : pc.getOwnedPartitions()) 
+			{
+				if (feature instanceof Part) 
+				{
+					val Part part = feature;
+					// Traitement des Composants fils du composant
+					val EObject type2 = part.getType();
+					if (type2 instanceof PhysicalComponent)
+					{
+						val EList<Part> deploying = part.getDeployingParts();
+						if (deploying == null || deploying.size() == 0)
+							list.add(part);
+					}
+				}
+			}
+		}
+		
+		// Traitement des Deployements
+		if (deployedLinkList != null && deployedLinkList.size() > 0)
+		{
+			for (AbstractDeploymentLink curDeploymentLink : deployedLinkList) 
+			{
+				val EObject deployedComponent = curDeploymentLink.getDeployedElement();
+				if (deployedComponent instanceof Part)
+					list.add(deployedComponent);
+			}
+		}
+
+		return list;
 	}
 	
 	def void print(){
--- Melange/org.sample.k3project/xtend-gen/org/sample/k3project/Mass.java
+++ Melange/org.sample.k3project/xtend-gen/org/sample/k3project/Mass.java
@@ -1,15 +1,23 @@
 package org.sample.k3project;
 
-import com.google.common.collect.Iterators;
+import com.google.common.base.Objects;
 import fr.inria.diverse.k3.al.annotationprocessor.Aspect;
-import java.util.Iterator;
-import org.eclipse.emf.common.util.TreeIterator;
+import java.util.List;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.Functions.Function2;
 import org.eclipse.xtext.xbase.lib.InputOutput;
-import org.eclipse.xtext.xbase.lib.IteratorExtensions;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.lib.ListExtensions;
+import org.polarsys.capella.common.data.modellingcore.AbstractType;
+import org.polarsys.capella.core.data.capellacore.Feature;
+import org.polarsys.capella.core.data.cs.AbstractDeploymentLink;
+import org.polarsys.capella.core.data.cs.Component;
 import org.polarsys.capella.core.data.cs.Part;
+import org.polarsys.capella.core.data.information.Partition;
+import org.polarsys.capella.core.data.pa.PhysicalComponent;
 import org.sample.k3project.MassPartAspectProperties;
 
 @Aspect(className = Part.class)
@@ -34,6 +42,13 @@ public class Mass {
     Object result = null;
     result = _privk3_computeMass(_self_, _self);;
     return (int)result;
+  }
+  
+  public static EList<Part> getSubComponentsObjects(final Part _self) {
+    final org.sample.k3project.MassPartAspectProperties _self_ = org.sample.k3project.MassPartAspectContext.getSelf(_self);
+    Object result = null;
+    result = _privk3_getSubComponentsObjects(_self_, _self);;
+    return (org.eclipse.emf.common.util.EList<org.polarsys.capella.core.data.cs.Part>)result;
   }
   
   public static void print(final Part _self) {
@@ -121,18 +136,85 @@ public class Mass {
   }
   
   protected static int _privk3_computeMass(final MassPartAspectProperties _self_, final Part _self) {
-    TreeIterator<EObject> _eAllContents = _self.eAllContents();
-    final Iterator<Part> subParts = Iterators.<Part>filter(_eAllContents, Part.class);
+    final EList<Part> subParts = Mass.getSubComponentsObjects(_self);
     final Function1<Part, Integer> _function = (Part it) -> {
       return Integer.valueOf(Mass.computeMass(it));
     };
-    Iterator<Integer> _map = IteratorExtensions.<Part, Integer>map(subParts, _function);
+    List<Integer> _map = ListExtensions.<Part, Integer>map(subParts, _function);
     final Function2<Integer, Integer, Integer> _function_1 = (Integer w1, Integer w2) -> {
       return Integer.valueOf(((w1).intValue() + (w2).intValue()));
     };
-    final Integer subPartWeight = IteratorExtensions.<Integer>reduce(_map, _function_1);
+    final Integer subPartWeight = IterableExtensions.<Integer>reduce(_map, _function_1);
     int _value = Mass.value(_self);
     return ((subPartWeight).intValue() + _value);
+  }
+  
+  protected static EList<Part> _privk3_getSubComponentsObjects(final MassPartAspectProperties _self_, final Part _self) {
+    final BasicEList<Part> list = new BasicEList<Part>();
+    EObject type = null;
+    EList<AbstractDeploymentLink> deployedLinkList = null;
+    if ((_self instanceof Part)) {
+      AbstractType _abstractType = _self.getAbstractType();
+      type = _abstractType;
+      EList<AbstractDeploymentLink> _deploymentLinks = _self.getDeploymentLinks();
+      deployedLinkList = _deploymentLinks;
+    } else {
+      if ((_self instanceof Component)) {
+        type = _self;
+      }
+    }
+    boolean _and = false;
+    boolean _notEquals = (!Objects.equal(type, null));
+    if (!_notEquals) {
+      _and = false;
+    } else {
+      _and = (type instanceof Component);
+    }
+    if (_and) {
+      final Component pc = ((Component) type);
+      EList<Partition> _ownedPartitions = pc.getOwnedPartitions();
+      for (final Feature feature : _ownedPartitions) {
+        if ((feature instanceof Part)) {
+          final Part part = ((Part)feature);
+          final EObject type2 = part.getType();
+          if ((type2 instanceof PhysicalComponent)) {
+            final EList<Part> deploying = part.getDeployingParts();
+            boolean _or = false;
+            boolean _equals = Objects.equal(deploying, null);
+            if (_equals) {
+              _or = true;
+            } else {
+              int _size = deploying.size();
+              boolean _equals_1 = (_size == 0);
+              _or = _equals_1;
+            }
+            if (_or) {
+              list.add(part);
+            }
+          }
+        }
+      }
+    }
+    boolean _and_1 = false;
+    boolean _notEquals_1 = (!Objects.equal(deployedLinkList, null));
+    if (!_notEquals_1) {
+      _and_1 = false;
+    } else {
+      int _size_1 = deployedLinkList.size();
+      boolean _greaterThan = (_size_1 > 0);
+      _and_1 = _greaterThan;
+    }
+    if (_and_1) {
+      for (final AbstractDeploymentLink curDeploymentLink : deployedLinkList) {
+        {
+          final EObject deployedComponent = curDeploymentLink.getDeployedElement();
+          if ((deployedComponent instanceof Part)) {
+            list.add(((Part)deployedComponent));
+          }
+        }
+      }
+    }
+    return list;
   }
   
   protected static void _privk3_print(final MassPartAspectProperties _self_, final Part _self) {
